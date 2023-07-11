31118db1f3c1b6c8e2b13d838d555f4e84dd3cb4
Raphael Bovi <93729728+RaphaelBovi@users.noreply.github.com>
2023-07-11 14:58:12 -0300

    start

---
+++ src/main/java/com/relatoriosclimaticos/RelatoriosClimaticosApplication.java
@@ -0,0 +1,13 @@
+package com.relatoriosclimaticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatoriosClimaticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatoriosClimaticosApplication.class, args);
+	}
+
+}
